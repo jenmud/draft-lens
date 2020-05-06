ae5366639a4ef76655bc70b9ad5633bc7e25de2f
Jenda Mudron <jenmud@gmail.com>
2020-05-06 06:47:46 +0000

    updating to use micro services

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jenmud/draft-lens
 go 1.14
 
 require (
-	github.com/jenmud/draft v0.0.0-20200504045458-c4ba27df586b // indirect
-	google.golang.org/grpc v1.29.1 // indirect
+	github.com/gobuffalo/packr/v2 v2.8.0 // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/jenmud/draft v0.0.0-20200506051429-9062311b96f2 // indirect
 )
--- main.go
+++ main.go
@@ -7,45 +7,64 @@ import (
 	"net/http"
 
 	pb "github.com/jenmud/draft/service"
-	"google.golang.org/grpc"
 
 	"github.com/gobuffalo/packr/v2"
 	"github.com/gorilla/mux"
+	"github.com/micro/go-micro/v2"
+	microConfig "github.com/micro/go-micro/v2/config"
+	microEnv "github.com/micro/go-micro/v2/config/source/env"
+	microFlag "github.com/micro/go-micro/v2/config/source/flag"
+	microWeb "github.com/micro/go-micro/v2/web"
 )
 
 var (
-	addr      = ":8080"
-	templates = packr.New("templates", "./templates")
-	static    = packr.New("static", "./static")
-	client    pb.GraphClient
+	version     = microWeb.Version("v0.0.0")
+	templates   = packr.New("templates", "./templates")
+	static      = packr.New("static", "./static")
+	config      microConfig.Config
+	draftClient pb.GraphService
 )
 
-// connectRPC connects the client to the gRPC server
-func connectRPC(addr string) error {
-	log.Printf("Connecting to RPC server %s", addr)
-	conn, err := grpc.Dial(
-		addr,
-		grpc.WithInsecure(),
-		grpc.WithBlock(),
+func parseArgs() {
+	var err error
+
+	config, err = microConfig.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.String("addr", ":", "Address to accept client connections on")
+	flag.String("name", "draft.web.srv", "Service name")
+	flag.String("draft-service", "draft.srv", "Draft service name")
+	flag.String("draft-addr", "", "Draft RPC server, if not given service discovery is used")
+	flag.Parse()
+
+	err = config.Load(
+		microEnv.NewSource(microEnv.WithPrefix("DRAFT_LENS")),
+		microFlag.NewSource(microFlag.IncludeUnset(true)),
 	)
 
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-
-	client = pb.NewGraphClient(conn)
-	log.Printf("Connected to RPC server %s", addr)
-	return nil
 }
 
 func init() {
-	flag.StringVar(&addr, "addr", addr, "Address and port to service and accept client connections.")
-	rpcAddr := flag.String("rpc-addr", ":8000", "RPC server to connect to and communicate with.")
-	flag.Parse()
+	parseArgs()
 
-	if err := connectRPC(*rpcAddr); err != nil {
-		log.Fatal(err)
+	var service micro.Service
+
+	srv := config.Get("draft", "addr").String("")
+	if srv != "" {
+		service = micro.NewService(micro.Address(srv))
+	} else {
+		service = micro.NewService(micro.Name(config.Get("draft", "service").String("draft.web.srv")))
 	}
+
+	draftClient = pb.NewGraphService(
+		config.Get("draft", "service").String("draft.web.srv"),
+		service.Client(),
+	)
 }
 
 // index serves up the index page.
@@ -70,7 +89,7 @@ func assetJSONQuery(w http.ResponseWriter, r *http.Request) {
 	query := r.FormValue("cypher")
 	log.Printf("Form data %v: %s", r.Form, query)
 
-	dump, err := client.Query(r.Context(), &pb.QueryReq{Query: query})
+	dump, err := draftClient.Query(r.Context(), &pb.QueryReq{Query: query})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -88,7 +107,7 @@ func assetJSONQuery(w http.ResponseWriter, r *http.Request) {
 
 // assetJSON serves JSON assets.
 func assetJSON(w http.ResponseWriter, r *http.Request) {
-	dump, err := client.Dump(r.Context(), &pb.DumpReq{})
+	dump, err := draftClient.Dump(r.Context(), &pb.DumpReq{})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -106,7 +125,7 @@ func assetJSON(w http.ResponseWriter, r *http.Request) {
 
 // statsJSON serves JSON stats.
 func statsJSON(w http.ResponseWriter, r *http.Request) {
-	dump, err := client.Stats(r.Context(), &pb.StatsReq{})
+	dump, err := draftClient.Stats(r.Context(), &pb.StatsReq{})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -123,18 +142,26 @@ func statsJSON(w http.ResponseWriter, r *http.Request) {
 }
 
 // run start the RPC service.
-func run(address string) error {
+func run() error {
 	router := mux.NewRouter()
+
 	router.HandleFunc("/", index).Methods("GET")
 	router.HandleFunc("/assets/json", assetJSON).Methods("GET")
 	router.HandleFunc("/assets/json", assetJSONQuery).Methods("POST")
 	router.HandleFunc("/stats/json", statsJSON)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(static)))
-	log.Printf("[%s] Service accepting connections on %s", "run", address)
-	return http.ListenAndServe(address, router)
+
+	service := microWeb.NewService(
+		microWeb.Name(config.Get("name").String("draft.web.srv")),
+		version,
+		microWeb.Address(config.Get("addr").String(":")),
+		microWeb.Handler(router),
+	)
+
+	return service.Run()
 }
 
 // main is the main entrypoint.
 func main() {
-	log.Fatal(run(addr))
+	log.Fatal(run())
 }
